c2f30cd0e5e3557b5c32a4b35169eaa547fbb9c3
J222 <baram789456@gmail.com>
2024-01-14 18:04:15 +0100

    [Refactor] Remove unused import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SynclipExceptionFilter } from './filters/synclipException.filter';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
